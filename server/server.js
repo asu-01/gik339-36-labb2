09ced4f2623c310d7af362a7fc694273b5c01e85
Oliver Näs <nolivern@gmail.com>
2023-12-08 15:51:18 +0100

    starting db

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-// Impporterar express
+// Impporterar express och sqlite3.
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 
-// Skapar en webbserver
+// Skapar en webbserver.
 const server = express();
 
-// Sätter inställningar för servern
+// Sätter inställningar för servern.
 server
     .use(express.json())
     .use(express.urlencoded({ extended: false }))
@@ -15,6 +16,17 @@ server
     next();
   });
 
+  // Öppnar servern på porten 3000.
   server.listen(3000, () => "Merry Christmas! The server is running on http://localhost/3000.");
 
-  server.get("/users", (req, res) => {});
+// get-metod för att ta serverns ska kunna ta emot förfrågningar mot databasen.
+  server.get("/users", (req, res) => {
+    // Öppnar upp en koppling till databasen.
+    let db = new sqlite3.Database("./gik339-labb2.db")
+    db.all(`SELECT * FROM USERS`, function(err, rows){
+      if(err){
+        return console.log(err.message);
+      }
+      res.send(rows);
+    });
+  });
