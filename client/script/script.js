f71680fbd23e8d02bc82520a3bb6d0f8a54bce75
Oliver Näs <nolivern@gmail.com>
2023-12-13 12:17:44 +0100

    Co-authored-by: asu-01 <asu-01@users.noreply.github.com> Co-authored-by: Alva Sundberg (Student) <h20alsun@du.se>

--- client/script/script.js
+++ client/script/script.js
@@ -1,16 +1,61 @@
 
 const localUrl = "http://localhost:3000/users" 
-const users = getUsers();
+/*const users = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(getUsers());
+  }, 100);
+});*/
+
+/*const users = getUsers();
 
 // Skapar uppkoppling mot server och översätter response-objektet till en array.
 async function getUsers () {
   const response = await fetch(localUrl);
   const users = await response.json();
+  console.log(users);
   return users;
-}
-console.log(users);
+}*/
 
+function loadUrl (url){
+  return new Promise(async function ( resolve, reject ) {
+    const getResolve = await fetch(url);
+
+    resolve(getResolve.json())
+
+    /* if (!resolve) {
+      reject(console.log("Hämtning av data misslyckades"));
+    } */
+  });
+}
+
+
+
+//Creates the 
 const createUl = `<ul> Hej </ul>`;
+document.body.insertAdjacentHTML("afterbegin", createUl);
+
+const promise = loadUrl(localUrl);
+
+// ---------------------------- //
+promise.then((users) => {
+  console.log(users);
+
+  
+  // Fortsätt här
+  users.forEach(user => {
+    const html = `<div class="list_item col-sm" style="border: 2px solid ${user.color}">
+    <p class="col-sm"> Username: ${user.username}  </p>
+    <p class="col-sm"> First Name: ${user.firstName} </p> 
+    <p class="col-sm"> Last Name: ${user.lastName}  </p>
+    </div>`;
+    console.log(html);
+
+    document.getElementById("listContainer").insertAdjacentHTML('beforeend', html);
+  });
+})
+// ------------------------- //
+
+/*const createUl = `<ul> Hej </ul>`;
 
 document.body.insertAdjacentHTML("afterbegin", createUl);
 
@@ -19,4 +64,10 @@ for (i = 0; i < users.length ; i++) {
   const createIl = `<il>${users[i]}</il>`;
   document.ul.insertAdjacentHTML("beforeend", createIl);
 }
+
+const setColor = document.getElementsByTagName("il");
+
+for (element in setColor) {
+  element.style.backgroundColor == users.color;
+}*/
 
